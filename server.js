f899b20676f68735fb0e42023641dc9f1e1ba64c
ShiKwan <31040895+ShiKwan@users.noreply.github.com>
2018-03-03 13:02:38 -0500

    created boiler plate

---
+++ server.js
@@ -0,0 +1,34 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let logger = require("morgan");
+let mongoose = require("mongoose");
+
+
+var PORT = 3000;
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+
+//consider using npr news to scrape. 
+
